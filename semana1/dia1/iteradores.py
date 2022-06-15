525084f4459d902d5c169f0ff0055411a019967c
JoelTirado <tiradojoel_523@gmail.com>
2022-06-14 22:19:51 -0500

    tareas

---
+++ semana1/dia1/iteradores.py
@@ -0,0 +1,16 @@
+
+#BUCLE FOR
+print("EJEMPLO DE BUCLE FOR : ")
+for contador in range(0,11,2):
+    print(contador)
+
+nombre = input("ingresa tu nombre : ")
+print(nombre[0:5])
+for contador in nombre:
+    print(contador)
+
+ #while   
+contador = 1
+while(contador < 11):
+    print(contador)
+    contador += 1
